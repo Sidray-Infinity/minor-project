4cacb8dc1de1f81f41b95817842e94241181ac9f
Siddharth Panda <siddharth.pandathedancer@gmail.com>
2020-01-30 17:33:06 +0530

    Update rename_files.py

--- Data/rename_files.py
+++ Data/rename_files.py
@@ -1,9 +1,9 @@
 import os
 
-DIR = "Data"
+DIR = "Images"
 
 count = 0
 for f in os.listdir(DIR):
     fname = os.path.join(DIR, f)
-    os.rename(fname, f"Data/img-{count}.jpg")
+    os.rename(fname, f"Images/img-{count}.jpg")
     count += 1
